57946e9f72cb0ade75ae09285731e40f5be17b2b
economy937 <economy9895@naver.com>
2024-11-05 12:15:56 +0900

    [Update]

--- Assets/Scripts/GridMaker.cs
+++ Assets/Scripts/GridMaker.cs
@@ -1,32 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GridMaker : MonoBehaviour
 {
-    public GameObject cellPrefab;  
-    public int rows = 5;           
-    public int columns = 5;        
+    public GameObject cellPrefab;
+    public int rows = 5;
+    public int columns = 5;
+    public Vector2 cellSpacing = new Vector2(5, 5);
+    public Vector2 cellSize = new Vector2(50, 50);
+
+    private GridLayoutGroup gridLayout;
 
     void Start()
     {
+        gridLayout = GetComponent<GridLayoutGroup>();
+        SetupGridLayout();
         GenerateGrid();
+    }
+
+    void SetupGridLayout()
+    {
+        gridLayout.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+        gridLayout.constraintCount = columns;
+        gridLayout.cellSize = cellSize;
+        gridLayout.spacing = cellSpacing;
     }
 
     void GenerateGrid()
     {
-
-        GridLayoutGroup gridLayout = GetComponent<GridLayoutGroup>();
-        gridLayout.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
-        gridLayout.constraintCount = columns;
+        ClearGrid();
 
         for (int i = 0; i < rows * columns; i++)
         {
             GameObject cell = Instantiate(cellPrefab, transform);
             cell.name = "Cell_" + i;
-            cell.AddComponent<CellController>();
         }
     }
 
+    void ClearGrid()
+    {
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+
+    void OnValidate()
+    {
+        if (rows < 1) rows = 1;
+        if (columns < 1) columns = 1;
+    }
 }
